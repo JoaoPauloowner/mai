49c11f231efb12d729c96b4122bfe79f4eaeb2c5
JoaoPauloowner <varimports13@gmail.com>
2026-09-22 13:06:13 -0300

    feat: export MAI design system primitives

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { AIAssistant } from "./AIAssistant";
+export { Button } from "./Button";
+export { Card, CardContent, CardHeader, CardTitle } from "./Card";
+export { DataTable, DataTableHeader, DataTableScroll } from "./DataTable";
+export { MetricCard } from "./MetricCard";
+export { PageHeader } from "./PageHeader";
+export { StatusBadge } from "./StatusBadge";
